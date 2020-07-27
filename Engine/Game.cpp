4bb0f723c0c70a4f48805d7df24e8f7b6c6a7163
Kushagra Agrawal <57141343+kushagraag@users.noreply.github.com>
2020-07-27 14:13:56 +0530

    make speed independent of frame rate

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -21,6 +21,7 @@
 #include "MainWindow.h"
 #include "Game.h"
 #include <random>
+#include "FrameTimer.h"
 
 Game::Game( MainWindow& wnd )
 	:
@@ -32,7 +33,7 @@ Game::Game( MainWindow& wnd )
 	goal( xDist( rng ),yDist( rng ) ),
 	meter( 20,20 )
 {
-	std::uniform_real_distribution<float> vDist( -2.5f,2.5f );
+	std::uniform_real_distribution<float> vDist( -2.5f * 60.0f ,2.5f * 60.0f );
 	for( int i = 0; i < nPoo; ++i )
 	{
 		poos[i].Init( xDist( rng ),yDist( rng ),vDist( rng ),vDist( rng ) );
@@ -50,15 +51,16 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
+	const float dt = ft.Mark();
 	goal.UpdateColor();
 	if( isStarted && !isGameOver )
 	{
-		dude.Update( wnd.kbd );
+		dude.Update( wnd.kbd, dt );
 		dude.ClampToScreen();
 
 		for( int i = 0; i < nPoo; ++i )
 		{
-			poos[i].Update();
+			poos[i].Update( dt );
 			if( poos[i].TestCollision( dude ) )
 			{
 				isGameOver = true;
