c7bddf9b579d0da93a6b737eebc6fcb6231a1666
Kushagra Agrawal <57141343+kushagraag@users.noreply.github.com>
2020-07-27 23:11:56 +0530

    Implement new functions in Vect2.h

--- Engine/Vect2.cpp
+++ Engine/Vect2.cpp
@@ -1,4 +1,5 @@
 #include "Vect2.h"
+#include <cmath>
 
 Vect2::Vect2(float x_in, float y_in)
 	:
@@ -25,4 +26,38 @@ Vect2 Vect2::operator*(float rhs) const
 Vect2& Vect2::operator*=(float rhs)
 {
 	return *this = *this * rhs;
+}
+
+Vect2 Vect2::operator-(const Vect2& rhs) const
+{
+	return Vect2( x - rhs.x, y - rhs.y);
+}
+
+Vect2& Vect2::operator-=(const Vect2& rhs)
+{
+	return *this = *this - rhs;
+}
+
+float Vect2::GetLength() const
+{
+	return std::sqrt(GetLengthSq());
+}
+
+float Vect2::GetLengthSq() const
+{
+	return x * x + y * y;
+}
+
+Vect2& Vect2::Normalize()
+{
+	return *this = GetNormalized();
+}
+
+Vect2 Vect2::GetNormalized() const
+{
+	const float len = GetLength();
+	if (len != 0.0f) {
+		return *this * (1.0f / len);
+	}
+	return *this;
 }
--- Engine/Vect2.h
+++ Engine/Vect2.h
@@ -9,6 +9,12 @@ public:
 	Vect2& operator+=(const Vect2& rhs);
 	Vect2 operator* (float rhs) const;
 	Vect2& operator*= (float rhs);
+	Vect2 operator-(const Vect2& rhs) const;
+	Vect2& operator-=(const Vect2& rhs);
+	float GetLength() const;
+	float GetLengthSq() const;
+	Vect2& Normalize();
+	Vect2 GetNormalized() const;
 
 public:
 	float x; 
